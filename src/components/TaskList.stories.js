9e4c28a0b3a8c840d44ddc1ccaf43d618eba8238
Hirokazu Okawa <h.okyawa@gmail.com>
2021-08-09 07:11:08 +0900

    Jest で単体テストする

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -17,12 +17,12 @@ Default.args = {
   // Shaping the stories through args composition.
   // The data was inherited from the Default story in task.stories.js.
   tasks: [
-    { ...TaskStories.Default.args.tasks, id: '1', title: 'Task 1' },
-    { ...TaskStories.Default.args.tasks, id: '2', title: 'Task 2' },
-    { ...TaskStories.Default.args.tasks, id: '3', title: 'Task 3' },
-    { ...TaskStories.Default.args.tasks, id: '4', title: 'Task 4' },
-    { ...TaskStories.Default.args.tasks, id: '5', title: 'Task 5' },
-    { ...TaskStories.Default.args.tasks, id: '6', title: 'Task 6' },
+    { ...TaskStories.Default.args.tasks, id: '1', title: 'Task 1', state: '' },
+    { ...TaskStories.Default.args.tasks, id: '2', title: 'Task 2', state: '' },
+    { ...TaskStories.Default.args.tasks, id: '3', title: 'Task 3', state: '' },
+    { ...TaskStories.Default.args.tasks, id: '4', title: 'Task 4', state: '' },
+    { ...TaskStories.Default.args.tasks, id: '5', title: 'Task 5', state: '' },
+    { ...TaskStories.Default.args.tasks, id: '6', title: 'Task 6', state: '' },
   ],
 };
 
